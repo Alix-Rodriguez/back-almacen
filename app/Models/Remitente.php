f104aa0b478abe69ec92733d10cd2c6dc50c8c6d
Alix-Rodriguez <alixm.081@gmail.com>
2023-05-05 20:22:57 -0400

    05/05

--- app/Models/Remitente.php
+++ app/Models/Remitente.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+
 class Remitente extends Model
 {
     public $table = 'remitentes';
