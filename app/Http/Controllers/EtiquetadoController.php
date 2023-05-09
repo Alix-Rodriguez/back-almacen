a84a853b8e71b67ba8cc6a3309eeb36b56b8d68d
keyprogm <98058445+keyprogm@users.noreply.github.com>
2023-05-09 14:38:28 -0400

    api etiquetado

--- app/Http/Controllers/EtiquetadoController.php
+++ app/Http/Controllers/EtiquetadoController.php
@@ -15,12 +15,12 @@ class EtiquetadoController extends Controller
             $etiquetado = new Etiquetado();
             $etiquetado -> nombre = $request -> nombre;
             $etiquetado -> archivo_img = $request -> archivo_img;
-            if($request->hasfile('archivo_img')):
+            /* if($request->hasfile('archivo_img')):
                 $img = getB64Image('archivo_img');
                 $img_extension = getB64Extension('archivo_img');
                 $img_name = 'img_etiquetado'. time() . '.' . $img_extension;
                 Storage::disk('images_base64')->put($img_name,$img);
-            endif;    
+            endif;  */   
             $etiquetado -> status = $request -> status;
             $etiquetado -> sistema = $request -> sistema;
             $etiquetado -> impresion = $request -> impresion;
@@ -37,5 +37,20 @@ class EtiquetadoController extends Controller
             ]);
 
         } */
+    }
+
+    public function listarRuta(){
+        try{
+            $Etiquetado = Etiquetado::all();
+            return response([
+                "status" => 200,
+                "data" => $Etiquetado
+            ]);
+        }catch(Exception $e){
+            return response([
+                "status" => 400,
+                "msn" => 'No se ha guardado - error'
+            ]);
+        }
     }
 }
