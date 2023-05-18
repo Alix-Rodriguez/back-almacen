9dcdc674db1fb1f0d097be17f44f0c47e13dbddb
Alix-Rodriguez <alixm.081@gmail.com>
2023-05-18 14:20:08 -0400

    composer instalado

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => 'dev-master',
-        'version' => 'dev-master',
-        'reference' => '4cf13ce36e521c043c9b650b41321b24dca9ac76',
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'reference' => 'ac021ebacbf0109a3df08f2fd12ddad59620c0b4',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -371,9 +371,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => 'dev-master',
-            'version' => 'dev-master',
-            'reference' => '4cf13ce36e521c043c9b650b41321b24dca9ac76',
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'reference' => 'ac021ebacbf0109a3df08f2fd12ddad59620c0b4',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
